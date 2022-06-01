87e81700cf368ff416ee40db288b06d00d3fc203
Pavel Arzhanau <p.arzhanau@softteco.com>
2022-06-01 15:27:16 +0300

    feat: implement auth (Срд 01 чэр 2022 15:27:16 +03)

---
+++ auth/signup.js
@@ -0,0 +1,67 @@
+import { useState } from 'react';
+import validateEmail from './utils/validateEmail';
+import validatePassword from './utils/validatePassword';
+import styles from './Signup.module.css';
+
+export default function SignUp({setIsPopupShowed, auth, createUserWithEmailAndPassword}) {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [isEmailErrorShowed, setIsEmailErrorShowed] = useState(false);
+    const [isPasswordErrorShowed, setIsPasswordErrorShowed] = useState(false);
+    const createUser = async (e) => {
+        e.preventDefault();
+        if (!validateEmail(email, process.env.NEXT_PUBLIC_ALLOWED_DOMAIN) || !validatePassword(password)) {
+            if (!validateEmail(email, process.env.NEXT_PUBLIC_ALLOWED_DOMAIN)) {
+                console.log(email);
+                console.log(process.env.NEXT_PUBLIC_ALLOWED_DOMAIN)
+                console.log(validateEmail(email, process.env.NEXT_PUBLIC_ALLOWED_DOMAIN));
+                setIsEmailErrorShowed(true);
+            }
+
+            if (!validatePassword(password)) {
+                setIsPasswordErrorShowed(true);
+            }
+
+            return;
+        }
+        try {
+            await createUserWithEmailAndPassword(auth, email, password);
+            setIsPopupShowed(false);
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const handleEmailInput = (e) => {
+        setIsEmailErrorShowed(false);
+        setEmail(e.target.value);
+    }
+
+    const handlePasswordInput = (e) => {
+        setIsPasswordErrorShowed(false);
+        setPassword(e.target.value);
+    }
+
+    const backToLogin = () => {
+        setIsPopupShowed(false);
+    }
+    
+    return (
+        <div className={styles.signupWrapper}>
+            <div className={styles.signup}>
+                <h2>Create an Account</h2>
+                <p>Lorem Ipsum is simply dummy text</p>
+                <form className={styles.signupForm}>
+                    <label htmlFor='email'>Email</label>
+                    <input type="email" value={email} onInput={handleEmailInput} id="email" />
+                    {isEmailErrorShowed ? <div className={styles.errorMessage}>Enter valid email</div> : null}
+                    <label htmlFor='password'>Password</label>
+                    <input type="password" value={password} onInput={handlePasswordInput} id="password" />
+                    {isPasswordErrorShowed ? <div className={styles.errorMessage}>Password must contain at least 8 symbols, at least 1 number, at least 1 capital</div> : null}
+                    <input className={styles.signupButton} type="submit" value="Create an Account" onClick={createUser} />
+                </form>
+                <div className={styles.loginAccount}>Already have an Account? <a onClick={backToLogin}>Login</a></div>
+            </div>
+        </div>
+    )
+}

9aec1e7388f6073da5ed22a86ced8214bc554608
Pavel Arzhanau <p.arzhanau@softteco.com>
2022-06-08 15:38:21 +0300

    fix: change firebase config (Срд 08 чэр 2022 15:38:21 +03)

--- auth/firebaseClient.js
+++ auth/firebaseClient.js
@@ -3,17 +3,17 @@ import {initializeApp, getApp} from 'firebase/app';
 export const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
     storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
     measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 export default function firebaseClient() {
     try {
-        getApp()
+        return getApp()
     } catch (err) {
-        initializeApp(firebaseConfig);
+        return initializeApp(firebaseConfig);
     }
 }
